9947827fffc5eb9f901569ead858b95225195e62
Irakli <51046973+iraklishub@users.noreply.github.com>
2023-11-17 21:43:40 +0400

    bg image test revert

--- src/app/components/themed-image/index.jsx
+++ src/app/components/themed-image/index.jsx
@@ -1,27 +1,13 @@
 'use client'
 
-import { useEffect, useState } from 'react'
 import { useTheme } from '@/src/utils/store'
 import Image from 'next/image'
 
 const ThemedImage = () => {
   const { background } = useTheme((state) => state.theme)
 
-  const [component, setComponent] = useState(() => (
-    <Image
-      src={background}
-      fill={true}
-      priority={true}
-      quality={100}
-      placeholder="blur"
-      alt="background"
-      sizes="100vw"
-      className="object-cover"
-    />
-  ))
-
-  useEffect(() => {
-    setComponent(() => (
+  return (
+    <div className="fixed w-screen h-screen -z-10 overflow-hidden">
       <Image
         src={background}
         fill={true}
@@ -32,10 +18,8 @@ const ThemedImage = () => {
         sizes="100vw"
         className="object-cover"
       />
-    ))
-  }, [background])
-
-  return <div className="fixed w-screen h-screen -z-10 overflow-hidden">{component}</div>
+    </div>
+  )
 }
 
 export default ThemedImage
