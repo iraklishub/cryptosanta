a81e82255b0745daa20b339b637e63de331c8bbc
Irakli <51046973+iraklishub@users.noreply.github.com>
2023-12-04 17:26:02 +0400

    theme switch icons fix and onclick func, ru text fix

--- src/app/components/icons/santa-icon.jsx
+++ src/app/components/icons/santa-icon.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import santaicon from '@/public/assets/icons/santa.png'
 
-const SantaIcon = () => <Image src={santaicon} width={40} height={40} alt="santaico" />
+const SantaIcon = () => <Image src={santaicon} width={30} height={30} alt="santaico" />
 
 export default SantaIcon
